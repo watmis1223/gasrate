8772e3d71b9ae77d947c08699be4820d0ac8dea1
Watcharapong Worakand <watcharapong@2c2p.com>
2018-11-11 05:07:10 +0700

    - implemented convert percent price - implemented convert special price - revised convert price functionals

--- Library/UI/PriceCalculation/CalculationBasicCtrl.cs
+++ Library/UI/PriceCalculation/CalculationBasicCtrl.cs
@@ -736,13 +736,6 @@ namespace CalculationOilPrice.Library.UI.PriceCalculation
         {
             if (calRow != null)
             {
-                //if convert needed
-                if (calRow.Convert != null)
-                {
-                    //if edited P then convert F
-                    calRow.Convert.ConvertAmountField = isPercent ? "F" : "P";
-                }
-
                 //if not master amount row
                 //master row's group is 0
                 if (calRow.Group != 0)
@@ -772,9 +765,20 @@ namespace CalculationOilPrice.Library.UI.PriceCalculation
             }
         }
 
-        void UpdateCalculationRowAmount(int rowID, decimal value, bool isPercent, bool specialCalculation)
+        void UpdateCalculationRowAmount(int rowID, decimal value, bool isPercent, bool specialCalculation, bool isCellEdit)
         {
             CalculationItemModel oCalRow = _Model.BasicCalculationItems[rowID];
+
+            //if convert needed
+            if (isCellEdit)
+            {
+                if (oCalRow.Convert != null)
+                {
+                    //if edited P then convert F
+                    oCalRow.Convert.ConvertAmountField = isPercent ? "F" : "P";
+                }
+            }
+
             UpdateCalulationRowAmount(oCalRow, value, isPercent, specialCalculation);
         }
 
@@ -783,6 +787,11 @@ namespace CalculationOilPrice.Library.UI.PriceCalculation
             CalculationItemModel oCalRow = _Model.BasicCalculationItems[rowID];
             if (oCalRow != null && oCalRow.Convert != null)
             {
+                // if not edit cell, set F as default when click convert button
+                if (String.IsNullOrWhiteSpace(oCalRow.Convert.ConvertAmountField))
+                {
+                    oCalRow.Convert.ConvertAmountField = "F";
+                }
                 oCalRow.Convert.Unit = unit;
             }
         }
@@ -866,7 +875,6 @@ namespace CalculationOilPrice.Library.UI.PriceCalculation
             }
 
             calRow.Total = calRow.AmountFix;
-            //calRow.UpdatedAmountField = "S";
         }
 
         void UpdateRowAmountPercent(CalculationItemModel calRow, decimal value, bool skipBaseGroupRows = false)
@@ -887,18 +895,41 @@ namespace CalculationOilPrice.Library.UI.PriceCalculation
             calRow.Total = calRow.AmountFix;
 
             //if convert needed
-            if (calRow.Convert != null && calRow.Convert.Unit == "VE")
+            //if edit P convert to F
+            if (calRow.Convert != null && !String.IsNullOrWhiteSpace(calRow.Convert.ConvertAmountField))
             {
-                if (_Model.GeneralSetting.Convert.VEUnitNumber > 0 &&
-                    //_Model.GeneralSetting.Convert.EEUnitNumber > 0 &&
-                    _Model.GeneralSetting.Convert.UnitNumber > 0)
+                if (calRow.Convert.ConvertAmountField == "F")
                 {
-                    if (calRow.Convert.ConvertAmountField == "F")
+                    if (calRow.Convert.Unit == "EE")
                     {
-                        calRow.AmountFix = calRow.AmountFix * (_Model.GeneralSetting.Convert.VEUnitNumber / _Model.GeneralSetting.Convert.UnitNumber);
+                        //calRow.Convert.OriginalAmount = calRow.AmountPercent;
+                        //int i = 0;
+                    }
+                    else if (calRow.Convert.Unit == "VE" && _Model.GeneralSetting.Convert.UnitNumber > 0)
+                    {
+                        calRow.AmountFix = calRow.AmountFix / _Model.GeneralSetting.Convert.UnitNumber;
                         calRow.Total = calRow.AmountFix;
                     }
                 }
+                else if (calRow.Convert.ConvertAmountField == "P")
+                {
+                    //calling from summary-all functional
+                    if (calRow.Convert.Unit == "EE")
+                    {
+                        //calRow.Convert.OriginalAmount = calRow.AmountPercent;
+                        calRow.AmountPercent = calRow.Convert.OriginalAmount;
+                        calRow.AmountFix = iBaseAmount * (calRow.AmountPercent / 100);
+                        calRow.Total = calRow.AmountFix;
+                    }
+                    else if (calRow.Convert.Unit == "VE" && _Model.GeneralSetting.Convert.UnitNumber > 0)
+                    {
+                        calRow.AmountPercent = calRow.Convert.OriginalAmount;
+                        calRow.AmountFix = iBaseAmount * (calRow.AmountPercent / 100);
+
+                        calRow.AmountPercent = calRow.AmountPercent / _Model.GeneralSetting.Convert.UnitNumber;
+                        calRow.Total = (iBaseAmount * (calRow.AmountPercent / 100));
+                    }
+                }                
             }
         }
 
@@ -922,17 +953,24 @@ namespace CalculationOilPrice.Library.UI.PriceCalculation
             }
             calRow.Total = calRow.AmountFix;
 
-            ////if convert needed
-            //if (calRow.Convert != null)
-            //{
-            //    calRow.Convert.ConvertAmountField = "P";
-            //    //if (_Model.GeneralSetting.Convert.UnitNumber > 0 && calRow.Convert.Unit == "VE")
-            //    //{
-            //    //    calRow.AmountPercent = calRow.AmountPercent / _Model.GeneralSetting.Convert.UnitNumber;
-            //    //}
-            //}
 
-            //calRow.UpdatedAmountField = "F";
+            //if convert needed
+            //if edit F convert to P
+            if (calRow.Convert != null && !String.IsNullOrWhiteSpace(calRow.Convert.ConvertAmountField))
+            {
+                if (calRow.Convert.ConvertAmountField == "P")
+                {
+                    if (calRow.Convert.Unit == "EE")
+                    {
+                        calRow.Convert.OriginalAmount = calRow.AmountPercent;
+                    }
+                    else if (calRow.Convert.Unit == "VE" && _Model.GeneralSetting.Convert.UnitNumber > 0)
+                    {
+                        calRow.AmountPercent = calRow.AmountPercent / _Model.GeneralSetting.Convert.UnitNumber;
+                        calRow.Total = (calRow.AmountPercent / 100) * iBaseAmount;
+                    }
+                }
+            }
         }
 
         decimal GetCalculationBaseSummaryGroups(List<int> calculationGroups)
@@ -983,7 +1021,7 @@ namespace CalculationOilPrice.Library.UI.PriceCalculation
                             bool isSpecial = false;
                             if (model.Group == 0)
                             {
-                                UpdateCalculationRowAmount(model.Order, model.AmountFix, false, isSpecial);
+                                UpdateCalculationRowAmount(model.Order, model.AmountFix, false, isSpecial, false);
                             }
                             else
                             {
@@ -998,24 +1036,48 @@ namespace CalculationOilPrice.Library.UI.PriceCalculation
                                 {
                                     if (_Model.GeneralSetting.PriceScale.MarkUp == "F")
                                     {
-                                        UpdateCalculationRowAmount(model.Order, model.AmountFix, false, isSpecial);
+                                        UpdateCalculationRowAmount(model.Order, model.AmountFix, false, isSpecial, false);
                                     }
                                     else
                                     {
-                                        UpdateCalculationRowAmount(model.Order, model.AmountPercent, true, isSpecial);
+                                        UpdateCalculationRowAmount(model.Order, model.AmountPercent, true, isSpecial, false);
                                     }
                                 }
                                 else
                                 {
                                     //if not use scale
-                                    UpdateCalculationRowAmount(model.Order, model.AmountPercent, true, isSpecial);
+                                    UpdateCalculationRowAmount(model.Order, model.AmountPercent, true, isSpecial, false);
+                                }
+                            }
+
+                            //if special field
+                            if (isSpecial)
+                            {
+                                //if convert needed for group
+                                if (model.Convert != null && model.Convert.Unit == "VE")
+                                {
+                                    if (_Model.GeneralSetting.Convert.UnitNumber > 0)
+                                    {
+                                        model.Total = model.Total / _Model.GeneralSetting.Convert.UnitNumber;
+                                    }
                                 }
                             }
                         }
                     }
 
-                    //update group total only
+                    //update group's total only
                     oGroup.Total += (from item in oModels select item.Total).Sum();
+                }
+
+                //if convert needed for group
+                if (oGroup.Convert != null && oGroup.Convert.Unit == "VE")
+                {
+                    if (//_Model.GeneralSetting.Convert.VEUnitNumber > 0 &&
+                        //_Model.GeneralSetting.Convert.EEUnitNumber > 0 &&
+                        _Model.GeneralSetting.Convert.UnitNumber > 0)
+                    {
+                        oGroup.Total = oGroup.Total / _Model.GeneralSetting.Convert.UnitNumber;
+                    }
                 }
             }
         }
@@ -1032,7 +1094,7 @@ namespace CalculationOilPrice.Library.UI.PriceCalculation
                 if (sTag == "BEK")
                 {
                     _Model.MasterAmount = Convert.ToDecimal(e.Value);
-                    UpdateCalculationRowAmount(e.RowHandle, _Model.MasterAmount, false, isSpecial);
+                    UpdateCalculationRowAmount(e.RowHandle, _Model.MasterAmount, false, isSpecial, true);
                 }
                 else
                 {
@@ -1044,11 +1106,11 @@ namespace CalculationOilPrice.Library.UI.PriceCalculation
 
                     if (e.Column.FieldName == TempColumnNames.AmountPercent.ToString())
                     {
-                        UpdateCalculationRowAmount(e.RowHandle, Convert.ToDecimal(e.Value), true, isSpecial);
+                        UpdateCalculationRowAmount(e.RowHandle, Convert.ToDecimal(e.Value), true, isSpecial, true);
                     }
                     else if (e.Column.FieldName == TempColumnNames.AmountFix.ToString())
                     {
-                        UpdateCalculationRowAmount(e.RowHandle, Convert.ToDecimal(e.Value), false, isSpecial);
+                        UpdateCalculationRowAmount(e.RowHandle, Convert.ToDecimal(e.Value), false, isSpecial, true);
                     }
                 }
 
